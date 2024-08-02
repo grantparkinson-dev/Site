947a1a4f170f098fce46e03d7120c9dad9bf3e82
mistercharred <Grant.c.parkinson@gmail.com>
2024-08-02 17:19:04 -0400

    viz fixes

--- projects/spotify-visualizations/js/spotify-viz.js
+++ projects/spotify-visualizations/js/spotify-viz.js
@@ -1,10 +1,15 @@
-// spotify-viz.js
 Promise.all([
     fetch('https://grantparkinson-dev.github.io/Site/projects/spotify-visualizations/data/artist_summary.json').then(response => response.json()),
     fetch('https://grantparkinson-dev.github.io/Site/projects/spotify-visualizations/data/song_summary.json').then(response => response.json())
   ]).then(([artistSummary, songSummary]) => {
-    createTopArtistsChart(artistSummary);
-    createTopTracksChart(songSummary);
+    // Sort artistSummary by total_ms_played in descending order and slice the top 10
+    const sortedArtistSummary = artistSummary.sort((a, b) => b.total_ms_played - a.total_ms_played).slice(0, 10);
+  
+    // Sort songSummary by total_ms_played in descending order and slice the top 10
+    const sortedSongSummary = songSummary.sort((a, b) => b.total_ms_played - a.total_ms_played).slice(0, 10);
+  
+    createTopArtistsChart(sortedArtistSummary);
+    createTopTracksChart(sortedSongSummary);
   });
   
   function createTopArtistsChart(data) {
@@ -12,34 +17,18 @@ Promise.all([
     const playTime = data.map(item => item.total_ms_played / 60000); // Convert to minutes
   
     const trace = {
-        x: artistNames,
-        y: playTime,
-        type: 'bar',
-        marker: {
-            color: 'rgba(50, 171, 96, 0.7)'
-        }
+      x: artistNames,
+      y: playTime,
+      type: 'bar',
+      marker: {
+        color: 'rgba(50, 171, 96, 0.7)'
+      }
     };
   
     const layout = {
-        title: 'Top Artists by Listening Time',
-        xaxis: { title: 'Artist' },
-        yaxis: { title: 'Listening Time (minutes)' },
-        updatemenus: [{
-            buttons: [
-                {
-                    method: 'restyle',
-                    args: ['y', [playTime]],
-                    label: 'All Time'
-                },
-                {
-                    method: 'restyle',
-                    args: ['y', [playTime.slice(0, 5)]],
-                    label: 'Top 5'
-                }
-            ],
-            direction: 'down',
-            showactive: true
-        }]
+      title: 'Top Artists by Listening Time',
+      xaxis: { title: 'Artist' },
+      yaxis: { title: 'Listening Time (minutes)' },
     };
   
     Plotly.newPlot('topArtistsChart', [trace], layout);
@@ -50,35 +39,19 @@ Promise.all([
     const playTime = data.map(item => item.total_ms_played / 60000); // Convert to minutes
   
     const trace = {
-        x: trackNames,
-        y: playTime,
-        type: 'bar',
-        marker: {
-            color: 'rgba(255, 153, 51, 0.7)'
-        },
-        orientation: 'h'
+      x: playTime,
+      y: trackNames,
+      type: 'bar',
+      marker: {
+        color: 'rgba(255, 153, 51, 0.7)'
+      },
+      orientation: 'h'
     };
   
     const layout = {
-        title: 'Top Tracks by Listening Time',
-        xaxis: { title: 'Listening Time (minutes)' },
-        yaxis: { title: 'Track' },
-        updatemenus: [{
-            buttons: [
-                {
-                    method: 'restyle',
-                    args: ['x', [playTime]],
-                    label: 'All Time'
-                },
-                {
-                    method: 'restyle',
-                    args: ['x', [playTime.slice(0, 5)]],
-                    label: 'Top 5'
-                }
-            ],
-            direction: 'down',
-            showactive: true
-        }]
+      title: 'Top Tracks by Listening Time',
+      xaxis: { title: 'Listening Time (minutes)' },
+      yaxis: { title: 'Track' },
     };
   
     Plotly.newPlot('topTracksChart', [trace], layout);
